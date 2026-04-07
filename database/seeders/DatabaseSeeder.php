055636f26eb6e1736ba9d86f18d57debe41c2ccf
Aristide Nikiema <Aristidenikiema046@gmail.com>
2026-04-07 12:18:57 +0200

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,25 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // On appelle le seeder de l'admin pour créer le compte super-admin
+        $this->call([
+            AdminUserSeeder::class,
         ]);
+
+        // Optionnel : Création d'un utilisateur de test standard
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
