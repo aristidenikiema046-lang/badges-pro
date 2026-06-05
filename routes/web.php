98c2cc3fa5beba1b76482ddb5770e96a0f0e5490
Aristide Nikiema <Aristidenikiema046@gmail.com>
2026-06-05 13:36:35 +0200

    reglage du bouton deconnexion

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,9 @@ use App\Http\Controllers\Admin\CompanyController;
 use App\Http\Controllers\BadgeExportController;
 
 // 1. ACCUEIL (Forcer la redirection vers le login pour éviter les conflits de dossier racine)
-Route::get('/', function () { 
+Route::any('/', function () { 
     return redirect()->route('login'); 
 })->name('home');
-
 // Sécurité : Si jamais une déconnexion tente un appel GET égaré
 Route::get('/logout', function () {
     auth()->logout();
